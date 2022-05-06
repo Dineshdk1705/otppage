27da2fb71520466dfc745de17138fde39a8623c2
dineshdk1705 <dnsh555g@gmail.com>
2022-05-06 16:04:06 +0530

    updated menubar  without responsiveness

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -4,26 +4,30 @@ import BookingsIcon from "../Icons/bookingsIcon";
 import ChevronDown from "../Icons/chevronDown";
 import RequestQuote from "./requestQuote";
 import WizIcon from "../Icons/wizIcon";
+
 import Button from "./button";
+import ShipIcon from "../Icons/shipIcon";
+import UserIcon from "../Icons/userIcon";
 
 const Navbar=()=>{
+    //5C20CF
     return (
         
-            <nav className="  pl-40 items-center bg-[#5C20CF] flex flex-row ">
+            <nav className="  items-center bg-[#5C20CF] flex flex-row">
                 <div className=" flex h-full m-0"  > 
-                <Button name=""iconLeft={<WizIcon/>}></Button>
+                    <div className="pl-10 pr-20 flex items-center">
+                       <WizIcon/>
+                    </div> 
 
-                    <Button name="WorkSpace" iconRight={<ChevronDown/>}></Button>
-                    <Button name="Quotes" iconLeft={<Quotes/>}></Button>
-                    <Button name="Bookings" iconLeft={<BookingsIcon/>}></Button>
-                    <Button name="Manage" iconLeft={<SettingsIcon/>} iconRight={<ChevronDown/>}></Button>
-                    </div>
-                    <div className="flex ml-80 m-0 h-full ">
-                        <RequestQuote />
-                    <Button name="Wiz Voyage" ></Button>
-
-
-                    <Button name="Admin" iconRight={<ChevronDown/>}></Button>
+                    <Button name="WorkSpace" iconRight={<ChevronDown/>} />
+                    <Button name="Quotes" iconLeft={<Quotes/>} />
+                    <Button name="Bookings" iconLeft={<BookingsIcon/>} />
+                    <Button name="Manage" iconLeft={<SettingsIcon/>} iconRight={<ChevronDown/>} />
+                </div>
+                <div className="flex ml-80 m-0 h-full ">
+                    <RequestQuote />
+                    <Button name="Wiz Voyage" iconLeft={<ShipIcon />} />
+                    <Button name="Admin" iconLeft={<UserIcon />} iconRight={<ChevronDown/>} />
                     </div>
                     {
       }
--- Components/button.tsx
+++ Components/button.tsx
@@ -3,8 +3,10 @@ import BookingsIcon from "../Icons/bookingsIcon";
 
 export default function Button(props) {
   return(
-     <div><button className=" flex flex-wrap bg-[#5C20CF] text-sm text-white  py-5 px-3 h-full hover:bg-[#5317C3]" > 
-     
-          {props.iconLeft}{props.name}{props.iconRight}</button></div> 
+    <div>
+      <button className=" flex items-center flex-wrap bg-[#5C20CF] text-sm text-white  py-5 px-3 h-full hover:bg-[#5317C3]" > 
+        {props.iconLeft}{props.name}{props.iconRight}
+      </button>
+    </div> 
   )
 }
